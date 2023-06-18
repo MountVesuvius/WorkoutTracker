882cec8ce36fd4ca3f27173f2093b0aa9dc4c452
MountVesuvius <JudeALakkis@gmail.com>
2023-06-18 12:21:20 +1000

    Check variants idea
    Just playing around with how I can implement button and selector
    variants. Haven't quite figured it out yet

---
+++ src/components/Check.svelte
@@ -0,0 +1,17 @@
+<script>
+	export let active = false;
+</script>
+
+<style lang="postcss">
+	.button { padding: 0.5rem; }
+    .active {background-color: theme(colors.green.300);}
+    .inactive {background-color: theme(colors.slate.200); background-opacity: 0.2;}
+</style>
+
+<button
+    class="w-8 rounded-md bg-opacity-20"
+	class:active={active===true}
+    class:inactive={active===false}
+>
+    ?
+</button>
