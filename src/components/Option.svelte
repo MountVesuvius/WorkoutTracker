36f90b8b448edbedb057c2eea3ac30eda168ddcf
MountVesuvius <JudeALakkis@gmail.com>
2023-06-18 18:08:24 +1000

    Working on exercise selection options

---
+++ src/components/Option.svelte
@@ -0,0 +1,12 @@
+<script>
+    export let exerciseName = "Bench Press";
+    export let exerciseCategory = "Chest";
+</script>
+
+<div class="flex border-2 border-white h-20 rounded-xl overflow-hidden m-2">
+    <img class="scale-1/4" src="https://dummyimage.com/300x200/de6fde/ffffff" alt="exercise display">
+    <div class="flex flex-col justify-center ml-3 text-left">
+        <h2 class="text-xl">{exerciseName}</h2>
+        <span class="text-gray-300 text-sm">{exerciseCategory}</span>
+    </div>
+</div>
---
+++ src/routes/selection/+page.svelte
@@ -0,0 +1,35 @@
+<script>
+    import Option from "../../components/Option.svelte";
+
+    import { exercises } from "../../stores/exercise";
+
+    let options = [
+        {id: 1, name: "Bench Press", category: "Chest"},
+        {id: 2, name: "Back Squat", category: "Legs"},
+        {id: 3, name: "Deadlift", category: "Back"},
+        {id: 4, name: "Overhead Press", category: "Shoulders"},
+        {id: 5, name: "Barbell Row", category: "Back"},
+    ]
+
+    function addExercise(name){
+        let l = $exercises.length;
+        if (l == 0) {
+            $exercises = [{id: l, name: name, sets: []}];
+        } else {
+            $exercises = [...$exercises, {id: l, name: name, sets: []}];
+        }
+        console.log($exercises);
+    }
+</script>
+
+<a href="/routine">
+    <button class="h-10">Return</button>
+</a>
+
+<div class="h-screen overflow-y-hidden scroll-auto">
+    {#each options as item}
+        <a href="/routine" on:click={() => { addExercise(item.name); }}>
+            <svelte:component this={Option} exerciseName={item.name} exerciseCategory={item.category}/>
+        </a>
+    {/each}
+</div>
